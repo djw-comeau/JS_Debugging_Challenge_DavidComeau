e3868cea4f61596fe547b62d403934f5ccc86dfe
David Comeau <w0158668@nscc.ca>
2025-02-23 20:06:23 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Student: David Comeau
+// ID: W0158668
+// Class: PROG2700 - Client Side Programming
+// Instructor: Nadia Gouda
+// Date: February 23, 2025
+
+/*
+
+Incorrect Code Template
+
 student_name = Nadia;
 studentAge == "20";
 isEnrolled = True; 
@@ -28,3 +38,40 @@ for (i = 1; i < 10; i++)
 
 while num < 10 
     num = prompt("Enter a number greater than 10")
+
+*/
+
+// Corrected Code
+
+let student_name = "Nadia";
+let studentAge = "20";
+let isEnrolled = true;
+
+document.write("<h2>Student Info</h2>");
+document.write("Name: " + student_name + "<br>");
+document.write("Age: " + studentAge + "<br>");
+document.write("Enrolled: " + isEnrolled + "<br>");
+
+let ageString = String(studentAge);
+document.write("Age Type: " + typeof ageString + "<br>");
+
+let num1 = Number(prompt("Enter first number"));
+let num2 = Number(prompt("Enter second number"));
+let total = num1 + num2;
+document.write("Sum: " + total + "<br>");
+
+let userAge = Number(prompt("Enter your age"));
+if (userAge === 18) {
+    document.write("You can vote!");
+} else if (userAge < 18) {
+    document.write("Sorry, you can't vote.");
+}
+
+for (let i = 1; i < 10; i++) {
+    document.write(i);
+}
+
+let num = 0;
+while (num < 10) {
+    num = Number(prompt("Enter a number greater than 10"));
+}
